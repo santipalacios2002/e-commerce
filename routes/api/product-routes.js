d7de638ac54cd83a1764b53c72aca3510ef1f332
Santiago Palacios <santipalacios2002@gmail.com>
2021-06-23 21:44:50 -0400

    added get one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,19 +8,27 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll({
+    const productsData = await Product.findAll({
       include: [{ model: Category}, { model: Tag, through: ProductTag}]
     });
-    res.status(200).json(productData);
+    res.status(200).json(productsData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag, through: ProductTag}]
+    });
+    res.status(200).json(productData)
+  } catch (error) {
+    
+  }
 });
 
 // create new product
