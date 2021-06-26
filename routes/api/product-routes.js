2cb8900d741c124d6a12fda6eeb3f153a3b50b25
Santiago Palacios <santipalacios2002@gmail.com>
2021-06-26 09:02:52 -0400

    added all delete routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,7 +41,6 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
     */
-   console.log('req.body:', req.body)
  Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -101,13 +100,26 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
